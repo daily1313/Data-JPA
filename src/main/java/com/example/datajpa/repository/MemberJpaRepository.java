2c1180b66ddc17b6c96fe3f0110f852daea5061f
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-13 21:13:12 +0900

    feat : Create 순수 JPA 기반 레포지토리 && CRUD Test

--- src/main/java/com/example/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/com/example/datajpa/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package com.example.datajpa.repository;
 
 import com.example.datajpa.entity.Member;
+import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
@@ -15,6 +17,27 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        // JPQL
+        List<Member> result = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        return result;
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 
     public Member find(Long id) {
