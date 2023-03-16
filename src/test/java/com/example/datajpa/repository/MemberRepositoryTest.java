3afef0525f63201a09b80e6bf612afaf61f13907
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-16 22:02:25 +0900

    feat : JPA Auditing 기능 추가(데이터 등록일, 수정일, 등록자, 수정자)

--- src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
@@ -312,6 +312,30 @@ public class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member);
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember = " + findMember.getCreatedDate());
+        System.out.println("findMember = " + findMember.getLastModifiedDate());
+        System.out.println("findMember = " + findMember.getCreatedBy());
+        System.out.println("findMember = " + findMember.getLastModifiedBy());
+
+
+    }
+
 
 
 
