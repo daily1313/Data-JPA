12cd9c65272829f2da16e42c43c64ca4c8f02676
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-17 18:20:51 +0900

    feat : Paging 기능 추가, Entity를 구별하기 위해 CreatedDate 어노테이션 추가

---
+++ src/main/java/com/example/datajpa/entity/Item.java
@@ -0,0 +1,43 @@
+package com.example.datajpa.entity;
+
+import static org.springframework.data.jpa.domain.AbstractAuditable_.createdDate;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
+
+    @Id
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+
+
+
+}
--- src/main/java/com/example/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/com/example/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -57,6 +59,29 @@ public class MemberJpaRepository {
                 .setParameter("username",username)
                 .getResultList();
     }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
+
+    public int bulkAgePlus(int age) {
+        return em.createQuery("update Member m set m.age = m.age + 1" +
+                        "where m.age >= :age")
+                .setParameter("age", age)
+                .executeUpdate();
+    }
+
+
 
 
 
